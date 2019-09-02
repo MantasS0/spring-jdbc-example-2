9aa568082d378c1d32215a1397ea4480dce33e03
MantasS0 <man_tas_pats@yahoo.com>
2019-09-02 15:59:13 +0300

    StudentService uses one mySQL query to get all of the data from DB, then uses streams to sort the result into individual student entities.

--- src/main/java/lt/bit/java2/springjdbc2/services/StudentService.java
+++ src/main/java/lt/bit/java2/springjdbc2/services/StudentService.java
@@ -1,10 +1,13 @@
 package lt.bit.java2.springjdbc2.services;
 
+import lt.bit.java2.springjdbc2.entities.Classes;
+import lt.bit.java2.springjdbc2.entities.Grade;
 import lt.bit.java2.springjdbc2.entities.Student;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 
 @Service
@@ -13,13 +16,13 @@ public class StudentService {
     private final JdbcTemplate jdbcTemplate;
     private final StudentClassGradeRowMapper rowMapper;
 
-    public StudentService(JdbcTemplate jdbcTemplate, StudentClassGradeRowMapper rowMapper){
+    public StudentService(JdbcTemplate jdbcTemplate, StudentClassGradeRowMapper rowMapper) {
         this.jdbcTemplate = jdbcTemplate;
         this.rowMapper = rowMapper;
     }
 
-    public List<Student> getAllStudents(){
-        return jdbcTemplate.query("SELECT students.id id, " +
+    public List<Student> getAllStudents() {
+        List<Student> studentList = jdbcTemplate.query("SELECT students.id id, " +
                 "students.name name, " +
                 "students.surname, " +
                 "students.email, " +
@@ -31,12 +34,38 @@ public class StudentService {
                 "grades.student_id g_stud_id, " +
                 "grades.class_id g_class_id " +
                 "FROM students " +
-                "LEFT OUTER JOIN student_class_relation scr ON students.id = scr.student_id " +
-                "INNER JOIN classes ON scr.class_id = classes.id " +
-                "INNER JOIN grades ON students.id = grades.student_id AND scr.class_id = grades.class_id " +
+                "LEFT OUTER JOIN student_classes scr ON students.id = scr.student_id " +
+                "INNER JOIN classes ON scr.classes_id = classes.id " +
+                "INNER JOIN grades ON students.id = grades.student_id AND scr.classes_id = grades.class_id " +
                 "ORDER BY students.id", rowMapper);
-    }
 
+        studentList = studentList.stream()
+                .filter(it -> it.getClasses() != null)
+                .collect(Collectors.groupingBy(Student::getId)).values().stream()
+                .map(studList -> {
+                    List<Classes> classes = studList.stream()
+                            .flatMap(stud -> stud.getClasses().stream())
+                            .collect(Collectors.toList());
+                    studList.get(0).setClasses(classes);
+                    return studList.get(0);
+                })
+                .collect(Collectors.toList()).stream()
+                    .map(stud -> {
+                        List<Classes> classesList = stud.getClasses().stream()
+                                .collect(Collectors.groupingBy(Classes::getId)).values().stream()
+                                .map(clList -> {
+                                    List<Grade> grades = clList.stream()
+                                            .flatMap(cl -> cl.getGrades().stream())
+                                            .collect(Collectors.toList());
+                                    clList.get(0).setGrades(grades);
+                                    return clList.get(0);
+                                }).collect(Collectors.toList());
+                        stud.setClasses(classesList);
+                        return stud;
+                    }).collect(Collectors.toList());
+
+        return studentList;
+    }
 
 
 }
--- src/main/resources/database.sql
+++ src/main/resources/database.sql
@@ -1,3 +1,58 @@
+# Student classes table
+CREATE TABLE `classes`
+(
+    `id`   int(10) unsigned                    NOT NULL AUTO_INCREMENT,
+    `name` varchar(45) COLLATE utf8_unicode_ci NOT NULL,
+    PRIMARY KEY (`id`)
+) ENGINE = InnoDB
+  AUTO_INCREMENT = 4
+  DEFAULT CHARSET = utf8 COLLATE = utf8_unicode_ci;
+
+# Student grades table
+CREATE TABLE `grades`
+(
+    `id`         int(10) unsigned NOT NULL AUTO_INCREMENT,
+    `grade`      int(10) unsigned NOT NULL,
+    `student_id` int(10) unsigned NOT NULL,
+    `class_id`   int(10) unsigned NOT NULL,
+    `date`       date             NOT NULL,
+    PRIMARY KEY (`id`),
+    KEY `FK_G_student_id_idx` (`student_id`),
+    KEY `FK_G_classes_id_idx` (`class_id`),
+    CONSTRAINT `FK_G_classes_id` FOREIGN KEY (`class_id`) REFERENCES `student_classes` (`classes_id`) ON DELETE CASCADE ON UPDATE CASCADE,
+    CONSTRAINT `FK_G_student_id` FOREIGN KEY (`student_id`) REFERENCES `student_classes` (`student_id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE = InnoDB
+  AUTO_INCREMENT = 59
+  DEFAULT CHARSET = utf8
+  COLLATE = utf8_unicode_ci;
+
+# Student-Classes relation table
+CREATE TABLE `student_classes`
+(
+    `student_id` int(10) unsigned NOT NULL,
+    `classes_id` int(10) unsigned NOT NULL,
+    PRIMARY KEY (`student_id`, `classes_id`),
+    KEY `FK_SC_classes_id` (`classes_id`),
+    CONSTRAINT `FK_SC_classes_id` FOREIGN KEY (`classes_id`) REFERENCES `classes` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+    CONSTRAINT `FK_SC_student_id` FOREIGN KEY (`student_id`) REFERENCES `students` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE = InnoDB
+  DEFAULT CHARSET = utf8
+  COLLATE = utf8_unicode_ci;
+
+# Student table
+CREATE TABLE `students`
+(
+    `id`      int(10) unsigned                    NOT NULL AUTO_INCREMENT,
+    `name`    varchar(45) COLLATE utf8_unicode_ci NOT NULL,
+    `surname` varchar(45) COLLATE utf8_unicode_ci NOT NULL,
+    `email`   varchar(45) COLLATE utf8_unicode_ci NOT NULL,
+    PRIMARY KEY (`id`)
+) ENGINE = InnoDB
+  AUTO_INCREMENT = 11
+  DEFAULT CHARSET = utf8
+  COLLATE = utf8_unicode_ci;
+
+# Insert primary data into tables
 INSERT INTO `school`.`classes` (`name`)
 VALUES ('Math_1');
 INSERT INTO `school`.`classes` (`name`)
@@ -26,47 +81,47 @@ VALUES ('Rupert', 'Armstrong', 'r.strong@mail.com');
 INSERT INTO `school`.`students` (`name`, `surname`, `email`)
 VALUES ('Jamie', 'Oliver', 'j.oliv@gmail.com');
 
-INSERT INTO `school`.`student_class_relation` (`student_id`, `class_id`)
+INSERT INTO `school`.`student_classes` (`student_id`, `classes_id`)
 VALUES (1, 1);
-INSERT INTO `school`.`student_class_relation` (`student_id`, `class_id`)
+INSERT INTO `school`.`student_classes` (`student_id`, `classes_id`)
 VALUES (1, 2);
-INSERT INTO `school`.`student_class_relation` (`student_id`, `class_id`)
+INSERT INTO `school`.`student_classes` (`student_id`, `classes_id`)
 VALUES (1, 3);
-INSERT INTO `school`.`student_class_relation` (`student_id`, `class_id`)
+INSERT INTO `school`.`student_classes` (`student_id`, `classes_id`)
 VALUES (2, 2);
-INSERT INTO `school`.`student_class_relation` (`student_id`, `class_id`)
+INSERT INTO `school`.`student_classes` (`student_id`, `classes_id`)
 VALUES (2, 3);
-INSERT INTO `school`.`student_class_relation` (`student_id`, `class_id`)
+INSERT INTO `school`.`student_classes` (`student_id`, `classes_id`)
 VALUES (3, 1);
-INSERT INTO `school`.`student_class_relation` (`student_id`, `class_id`)
+INSERT INTO `school`.`student_classes` (`student_id`, `classes_id`)
 VALUES (4, 2);
-INSERT INTO `school`.`student_class_relation` (`student_id`, `class_id`)
+INSERT INTO `school`.`student_classes` (`student_id`, `classes_id`)
 VALUES (4, 1);
-INSERT INTO `school`.`student_class_relation` (`student_id`, `class_id`)
+INSERT INTO `school`.`student_classes` (`student_id`, `classes_id`)
 VALUES (5, 3);
-INSERT INTO `school`.`student_class_relation` (`student_id`, `class_id`)
+INSERT INTO `school`.`student_classes` (`student_id`, `classes_id`)
 VALUES (5, 1);
-INSERT INTO `school`.`student_class_relation` (`student_id`, `class_id`)
+INSERT INTO `school`.`student_classes` (`student_id`, `classes_id`)
 VALUES (6, 2);
-INSERT INTO `school`.`student_class_relation` (`student_id`, `class_id`)
+INSERT INTO `school`.`student_classes` (`student_id`, `classes_id`)
 VALUES (7, 1);
-INSERT INTO `school`.`student_class_relation` (`student_id`, `class_id`)
+INSERT INTO `school`.`student_classes` (`student_id`, `classes_id`)
 VALUES (7, 2);
-INSERT INTO `school`.`student_class_relation` (`student_id`, `class_id`)
+INSERT INTO `school`.`student_classes` (`student_id`, `classes_id`)
 VALUES (7, 3);
-INSERT INTO `school`.`student_class_relation` (`student_id`, `class_id`)
+INSERT INTO `school`.`student_classes` (`student_id`, `classes_id`)
 VALUES (8, 1);
-INSERT INTO `school`.`student_class_relation` (`student_id`, `class_id`)
+INSERT INTO `school`.`student_classes` (`student_id`, `classes_id`)
 VALUES (8, 3);
-INSERT INTO `school`.`student_class_relation` (`student_id`, `class_id`)
+INSERT INTO `school`.`student_classes` (`student_id`, `classes_id`)
 VALUES (9, 2);
-INSERT INTO `school`.`student_class_relation` (`student_id`, `class_id`)
+INSERT INTO `school`.`student_classes` (`student_id`, `classes_id`)
 VALUES (9, 3);
-INSERT INTO `school`.`student_class_relation` (`student_id`, `class_id`)
+INSERT INTO `school`.`student_classes` (`student_id`, `classes_id`)
 VALUES (10, 1);
-INSERT INTO `school`.`student_class_relation` (`student_id`, `class_id`)
+INSERT INTO `school`.`student_classes` (`student_id`, `classes_id`)
 VALUES (10, 2);
-INSERT INTO `school`.`student_class_relation` (`student_id`, `class_id`)
+INSERT INTO `school`.`student_classes` (`student_id`, `classes_id`)
 VALUES (10, 3);
 
 INSERT INTO `school`.`grades` (`grade`, `student_id`, `class_id`, `date`)
@@ -127,3 +182,22 @@ INSERT INTO `school`.`grades` (`grade`, `student_id`, `class_id`, `date`)
 VALUES (5, 1, 3, '2019-08-27');
 INSERT INTO `school`.`grades` (`grade`, `student_id`, `class_id`, `date`)
 VALUES (6, 10, 1, '2019-08-27');
+
+
+### Select statements ###
+SELECT students.id       id,
+       students.name     name,
+       students.surname,
+       students.email,
+       classes.id        c_id,
+       classes.name      c_name,
+       grades.id         g_id,
+       grade,
+       grades.date       g_date,
+       grades.student_id g_stud_id,
+       grades.class_id   g_class_id
+FROM students
+         LEFT OUTER JOIN student_classes scr ON students.id = scr.student_id
+         INNER JOIN classes ON scr.classes_id = classes.id
+         INNER JOIN grades ON students.id = grades.student_id AND scr.classes_id = grades.class_id
+ORDER BY students.id;
